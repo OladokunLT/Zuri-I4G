1431e454cf83b2b889686a1bedd022df78d1bd0d
Oladokun Lukman Tunde <lukmanoladokun86@gmail.com>
2022-06-22 00:25:02 +0100

    Initial commit - Myslef: name, height and country

---
+++ javascript/myself.js
@@ -0,0 +1,7 @@
+    //declaring variables
+let fullName = "Lukman Oladokun";
+let height = "5.2m";
+let country = "Nigeria";
+
+    // print to screen(console)
+console.log (fullName + "\n"+ height + "\n" + country );
